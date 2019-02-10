8ba2439f68eaff5a9c29935929d5b21397e5b3a3
Matthew Matl <matthewcmatl@gmail.com>
2019-02-10 13:50:51 -0800

    Fix doc import

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,12 +15,12 @@
 import sys
 import os
 import sphinx_rtd_theme
-from pyrender import __version__
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('../../'))
+from pyrender import __version__
 
 # -- General configuration ------------------------------------------------
 
