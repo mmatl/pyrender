806f36e7263f259a96f25449b5793791decf3c8a
Frederik Hagelskjær <frederikhagel@gmail.com>
2024-03-13 07:16:20 +0100

    Changed RenderFlags.FLAT to also disable anti alias

--- pyrender/renderer.py
+++ pyrender/renderer.py
@@ -335,7 +335,7 @@ class Renderer(object):
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
-        if not bool(flags & RenderFlags.SEG):
+        if not bool(flags & RenderFlags.SEG or flags & RenderFlags.FLAT):
             glEnable(GL_MULTISAMPLE)
         else:
             glDisable(GL_MULTISAMPLE)
